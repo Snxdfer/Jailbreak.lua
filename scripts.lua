e73e916f48b818d5f446ef632a14400b7f32cecd
Snxdfer <121313122+Snxdfer@users.noreply.github.com>
2024-07-05 21:54:35 -0400

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/JB-esp/main/esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/keycard.lua/main/keycard.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/jbsilentaim/main/silentaim.lua"))()
